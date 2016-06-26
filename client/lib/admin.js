000b998b8e9170fcfaf0c13d2faef387a8d04acd
Precari Web Dev <web.dev@precari.co>
2016-06-26 15:46:57 +0600

    Moved admin emails to server config

--- client/lib/admin.js
+++ client/lib/admin.js
@@ -1,11 +1,11 @@
 
 /**
- * Admin config file for Meteor Admin (yogiben:admin)
+ * Clientside Admin config for Meteor Admin (yogiben:admin)
  * https://github.com/yogiben/meteor-admin
+ * Sensitive/server AdminConfig data is located in: /server/lib/admin.js
  */
 AdminConfig = {
-  name: Meteor.settings.public.yogiben_admin.appName,
-  adminEmails: Meteor.settings.public.yogiben_admin.adminEmails,
+  name: Meteor.settings.public.siteName + ' Admin',
   collections: {
     Posts: {},
     Comments: {},
---
+++ server/lib/admin.js
@@ -0,0 +1,9 @@
+
+/**
+ * Secure admin config for Meteor Admin (yogiben:admin)
+ * https://github.com/yogiben/meteor-admin
+ * Client AdminConfig data is located in: /client/lib/admin.js
+ */
+AdminConfig = {
+  adminEmails: Meteor.settings.yogiben_admin.adminEmails,
+};
